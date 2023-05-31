51cbaddeb8f4b8954b8fd302f7f7b7a5711d6e92
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-05-31 12:39:34 +0200

    Math EquationAnswer model testing

---
+++ src/test/kotlin/org/maurycy/framework/math/model/EquationAnswerTest.kt
@@ -0,0 +1,35 @@
+package org.maurycy.framework.math.model
+
+import org.junit.jupiter.api.Test
+import org.wildfly.common.Assert
+
+class EquationAnswerTest {
+    private val solution1 = doubleArrayOf(5.0,4.0,3.0)
+    private val solution2 = doubleArrayOf(5.0,4.0,3.0)
+    private val solution3 = doubleArrayOf(5.0,4.0,3.1)
+    @Test
+    fun testEquals1() {
+    Assert.assertTrue(EquationAnswer(solution1) == EquationAnswer(solution1))
+    }
+    @Test
+    fun testEquals2() {
+        Assert.assertTrue(EquationAnswer(solution1) == EquationAnswer(solution2))
+    }
+    @Test
+    fun testEquals3() {
+        Assert.assertFalse(EquationAnswer(solution1) == EquationAnswer(solution3))
+    }
+
+    @Test
+    fun testHashCode1() {
+        Assert.assertTrue(EquationAnswer(solution1).hashCode() == EquationAnswer(solution1).hashCode())
+    }
+    @Test
+    fun testHashCode2() {
+        Assert.assertTrue(EquationAnswer(solution1).hashCode() == EquationAnswer(solution2).hashCode())
+    }
+    @Test
+    fun testHashCode3() {
+        Assert.assertFalse(EquationAnswer(solution1).hashCode() == EquationAnswer(solution3).hashCode())
+    }
+}
